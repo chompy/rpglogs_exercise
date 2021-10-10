67c8f1dbe7d59151e03be750d2e56be86f8e77c1
Nathan Ogden <nathan@ogden.tech>
2021-10-10 17:24:13 -0400

    updated .env.example and added cp -> .env in .platform.app.yaml, added github link in page head

--- resources/views/app.blade.php
+++ resources/views/app.blade.php
@@ -4,18 +4,27 @@
         <meta charset="utf-8" />
         <meta http-equiv="X-UA-Compatible" content="IE=edge" />
         <meta name="viewport" content="width=device-width, initial-scale=1" />
-        <meta name="csrf-token" content="{{ csrf_token() }}" />
-        <title>Nathan RPGLogs Exercise</title>
-        <link href="{{ asset('css/app.css') }}" rel="stylesheet" />
+        <title>{{ env('APP_NAME') }}</title>
+        <link href="/css/app.css" rel="stylesheet" />
     </head>
     <body>
         <div id="container">
             <div id="head">
-                <a href="/" class="title">Nathan RPGLogs Exercise</a>
+                <div class="left">
+                    <a href="/" class="title">{{ env('APP_NAME') }}</a>
+                </div>
+                <div class="right">
+                    <a href="https://github.com/chompy/rpglogs_exercise" target="_blank">
+                        <img 
+                            src="/img/github.png"
+                            alt="github.com/chompy/rpglogs_exercise"
+                            title="github.com/chompy/rpglogs_exercise"
+                        />
+                    </a>
+                </div>
             </div>
-
             <div id="app"></div>
         </div>
-        <script src="{{ asset('js/app.js') }}"></script>
+        <script src="/js/app.js"></script>
     </body>
 </html>
